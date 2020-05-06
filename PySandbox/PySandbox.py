6a5469e215bba8e94994d6ff56b4125f5d38f42a
jmckeever79 <jj@jjmckeever.com>
2020-05-05 21:24:05 -0400

    linked list problem

--- PySandbox/PySandbox.py
+++ PySandbox/PySandbox.py
@@ -1,6 +1,6 @@
 import math
 import numpy as np
-from leetcode import movezeroes
+from leetcode import movezeroes, run_mod
 
 def run(nums):
     result={}
@@ -38,73 +38,7 @@ def extract_digits(num):
         i += 1
     return result
 
-def compute_max_contig(nums):
-    l = len(nums)
-    if l == 1:
-        return nums[0]
-
-    s = sum(nums)
-    result = nums
-
-    for i in range(0, l+1):
-        for j in range(i, l+1):
-            print('<{0},{1}>'.format(i,j))
-            temp = nums[i:j]
-            if len(temp) == 0:
-                continue
-            tempsum = sum(temp)
-            if tempsum > s:
-                s = tempsum
-                result = temp
-
-    return s
-
-def max_crossing_sum(v, startix, mid, endix):
-    print('Computing crossing sum for {0}--{1} at {2}'.format(startix, endix, mid))
-
-    sm = 0
-    leftsum = -math.inf
-    r0 = range(mid, startix, -1)
-    print('Computing left sum for {0}'.format(r0))
-    for i in r0:
-        sm = sm + v[i]
-        if sm > leftsum:
-            leftsum = sm
-
-    sm = 0
-    rightsum = -math.inf
-    r1 = range(mid+1, endix)
-    print('Computing right sum for {0}'.format(r1))
-    for i in r1:
-        sm = sm + v[i]
-        if sm > rightsum:
-            rightsum = sm
-
-    print('Left sum is {0} and right sum is {1}'.format(leftsum, rightsum))
-    return max(leftsum+rightsum, leftsum, rightsum)
-
-
-def max_subarray_sum(vector, startix, endix):
-    print('Computing subarray sum for {0} to {1}'.format(startix, endix))
-
-    if startix == endix:
-        print('Reached 1-d vector at {0} = {1}'.format(startix, vector[startix]))
-        return vector[startix]
-
-    mid = (startix + endix) // 2
-
-    lh = max_subarray_sum(vector, startix, mid)
-    rh = max_subarray_sum(vector, mid+1, endix)
-    cr = max_crossing_sum(vector, startix, mid, endix)
-
-    print('Vector is {0}'.format(v))
-    print('Left sum is {0}, right sum is {1}, crossing sum is {2}'.format(lh, rh, cr))
-
-    return max([lh, rh, cr])
-
 #print(run([25,14,7,1,22,25,22,7,1]))
 #print(happy_number(19))
 
-nums = [0,0,0,0,0]
-print(movezeroes(nums))
-
+run_mod()
--- PySandbox/leetcode.py
+++ PySandbox/leetcode.py
@@ -1,5 +1,33 @@
 import math
 
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+        return '{0}'.format(self.val)
+
+def linkedlist_middlenode(head):
+    if head.next is None:
+        return head
+    d = {}
+    i = 1
+    while head.next is not None:
+        d[i] = head.val
+        head = head.next
+        i += 1
+    return d[i//2 + 1]
+
+def run_mod():
+    l = ListNode(1)
+    l.next = ListNode(2)
+    l.next.next = ListNode(3)
+    l.next.next.next = ListNode(4)
+    l.next.next.next.next = ListNode(5)
+    print(linkedlist_middlenode(l))
+    return
+
 def movezeroes(nums):
     l = len(nums)
     i = 0
